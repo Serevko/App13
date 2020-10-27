c4bb1e36cb5ed728de10d1e4850d9670df2cddb5
dsadasdas dsadsadas <mike13.lv@gmail.com>
2020-10-28 01:16:56 +0200

    added docstring to SpeedAdapter class

--- cars/adapter.py
+++ cars/adapter.py
@@ -1,9 +1,44 @@
 
 class SpeedAdapter:
-    """Speed Adapter class to limit speed of other cars to 50"""
+    """
+    A class to limit speed of the car.
+
+    ...
+
+    Attributes
+    ----------
+    name : str
+        name of the car
+
+    Methods
+    -------
+    speed():
+        Limits speed of the car to 50.
+    """
+
     def __init__(self, name):
+        """
+        Constructs all the necessary attributes for the SpeedAdapter object.
+
+        Parameters
+        ----------
+            name : str
+                name of the car
+            city_speed : int
+                      city limit(50)
+        """
+
         self.name = name
         self.city_speed = 50
 
     def speed(self):
+        """
+        Returns the SpeedAdapter object with city_speed
+
+        Returns
+        -------
+        city_speed : int
+            limits speed to 50
+        """
+
         return self.city_speed
